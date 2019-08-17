279335938783b86028f1354574f92c4bb65517ac
Andrea Cerasoni <andreacerasoni@gmail.com>
2019-08-17 20:50:19 +0100

    Empty project

---
+++ desktop/src/com/mygdx/sos/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.sos.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.sos.SosMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SosMain(), config);
+	}
+}
